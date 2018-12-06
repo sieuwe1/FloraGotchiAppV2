92937036de053d2be946da2d6e29af176ba75033
sieuwe elferink <sieuwe_crossen@hotmail.com>
2018-12-06 13:04:57 +0100

    Initial upload

---
+++ FloraGotchiAppV2/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FloraGotchiAppV2.Controllers
+{
+    public class HomeController : Controller
+    {
+        int HumLv = 87;
+        int temp = 22;
+        GameScore s;
+
+        public HomeController()
+        {
+            s = new GameScore();
+        }
+
+        public IActionResult Index()
+        {
+            ViewData["GameScore"] = "Score = " + s.GetScore();
+            ViewData["HumLv"] = "Humidity level = " + HumLv + "%";
+            ViewData["Temp"] = "temperature = " + temp + "degrees";
+
+            return View();
+        }
+
+        public IActionResult Page1(int AmountToAdd)
+        {
+
+            s.AddScore(AmountToAdd);
+
+            ViewData["GameScore"] = "Score = " + s.GetScore();
+
+            return View();
+        }
+
+
+
+    }
+}
---
+++ FloraGotchiAppV2/GameScore.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FloraGotchiAppV2
+{
+    public class GameScore
+    {
+
+        int score = 5000;
+
+        public GameScore()
+        {
+                
+        }
+
+        public void AddScore(int add)
+        {
+            score = score + add;
+
+        }
+
+        public int GetScore()
+        {
+            
+            return score;
+
+        }
+
+    }
+}
