8cd949a7b5b77b3512df23e87e157df407df6d7f
maluvdberg <malu.vandenberg@student.fontys.nl>
2019-01-08 00:00:37 +0100

    layout structure

--- FloraGotchiAppV2/wwwroot/js/site.js
+++ FloraGotchiAppV2/wwwroot/js/site.js
@@ -2,3 +2,28 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$(document).ready(function () {
+
+    $('nav').prepend('<div id="responsive-nav" style="display:none">Icon</div>');
+    $('#responsive-nav').on('click', function () {
+        $('nav ul').toggle("slide");
+    });
+
+    $(window).resize(function () {
+
+        if ($(window).innerWidth() < 768) {
+            $('nav ul li').css('display', 'block');
+            $('nav ul').hide()
+            $('#responsive-nav').show()
+        } else {
+            $('nav ul li').css('display', 'inline-block');
+            $('nav ul').show()
+            $('#responsive-nav').hide()
+        }
+
+    });
+
+    $(window).resize();
+
+});
